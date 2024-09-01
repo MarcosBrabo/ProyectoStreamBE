d9dde8f991c2bae3f3ba521615f719cb4a43c8a4
Gonzalo Casañas <gonzalofabiansendra@gmail.com>
2024-09-01 02:12:50 -0300

    Se creo Contactos y verificacion
    Se creo el sistema de contacto donde tambien se verifica si los datos ingresados son los adecuados.

--- apporganization/src/index.js
+++ apporganization/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
-import Contact from './Componentes-pag-principal/Contact'; // Asegúrate de que Contact.js está en el directorio correcto y con el nombre correcto
-import About from './Componentes-pag-principal/About'; // Asegúrate de que About.js está en el directorio correcto y con el nombre correcto
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Contact from './Componentes-pag-principal/Contact'; 
+import About from './Componentes-pag-principal/About'; 
 
 ReactDOM.render(
   <Router>
